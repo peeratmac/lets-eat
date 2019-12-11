abea41e22c7d1e155065cce206e8b76508061689
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-11 09:47:58 -0700

    Added handleChange method to update FormInput states accordingly based on user inputs.

--- src/FormInput/FormInput.js
+++ src/FormInput/FormInput.js
@@ -11,28 +11,36 @@ class FormInput extends Component {
     };
   }
 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
     return (
       <div>
         <input
+          onChange={this.handleChange}
           type='text'
           name='name'
           value={this.state.name}
           placeholder='Name'
         />
         <input
+          onChange={this.handleChange}
           type='text'
           name='date'
           value={this.state.date}
           placeholder='Date (MM/DD)'
         />
         <input
+          onChange={this.handleChange}
           type='text'
-          name='name'
+          name='time'
           value={this.state.time}
           placeholder='Time (X:XX am/pm)'
         />
         <input
+          onChange={this.handleChange}
           type='text'
           name='number'
           value={this.state.number}
