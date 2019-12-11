a516bd7cf6be18e84b033b0392fd26616dba9b4a
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-11 11:55:27 -0700

    Add async methods testing for POST and DELETE requests.  And more tests in App Component.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import ReactDOM from 'react-dom';
+import { cancelReservation } from './App';
 
 describe('APP', () => {
   it('renders without crashing', () => {
@@ -13,5 +14,132 @@ describe('APP', () => {
   it('should match the App Component Snapshot', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should retrieve all reservations data on the server upon mounting', async () => {
+    const wrapper = shallow(<App />);
+
+    shallow(<App />);
+
+    const mockReservationsData = [
+      { date: '12/25', id: 1, name: 'Peerat', number: 55, time: '7:00 am' }
+    ];
+
+    const mockResponse = {
+      allReservationsData: mockReservationsData
+    };
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+
+    const instance = wrapper.instance();
+    await instance.componentDidMount();
+
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost:3001/api/v1/reservations'
+    );
+  });
+
+  it('should call fetch to make a POST request with correct arguments', () => {
+    const wrapper = shallow(<App />);
+    const expected = [
+      'http://localhost:3001/api/v1/reservations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          date: '12/25',
+          id: 1,
+          name: 'Peerat',
+          number: 55,
+          time: '7:00 am'
+        })
+      }
+    ];
+
+    wrapper.instance().makeReservation({
+      date: '12/25',
+      id: 1,
+      name: 'Peerat',
+      number: 55,
+      time: '7:00 am'
+    });
+
+    expect(window.fetch).toHaveBeenCalledWith(...expected);
+  });
+
+  it('should call fetch to make a DELETE request with correct arguments', () => {
+    const wrapper = shallow(<App />);
+    const expected = [
+      `http://localhost:3001/api/v1/reservations/1`,
+      {
+        method: 'DELETE'
+      }
+    ];
+
+    wrapper.instance().cancelReservation(1);
+
+    expect(window.fetch).toHaveBeenCalledWith(...expected);
+  });
+
+  it.skip('should update state with a new reservation when makeReservation is called', async () => {
+    const wrapper = shallow(<App />);
+
+    const mockReservationsData = [
+      { date: '12/25', id: 1, name: 'Peerat', number: 55, time: '7:00 am' }
+    ];
+
+    const mockResponse = {
+      allReservationsData: mockReservationsData
+    };
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+
+    wrapper.instance().makeReservation = jest.fn();
+
+    wrapper.instance().makeReservation.mockImplementation(() => {
+      return Promise.resolve({
+        date: '12/24',
+        id: 5782,
+        name: 'Maria Sharapova',
+        number: 60,
+        time: '5:00 am'
+      });
+    });
+
+    const mockNewReservation = {
+      date: '12/24',
+      id: 6654,
+      name: 'Serena Williams',
+      number: 70,
+      time: '6:00 am'
+    };
+
+    const expected = [
+      {
+        date: '12/24',
+        id: 5782,
+        name: 'Maria Sharapova',
+        number: 60,
+        time: '5:00 am'
+      },
+      mockNewReservation
+    ];
+
+    await wrapper.instance().makeReservation(mockNewReservation);
+
+    expect(wrapper.instance().makeReservation).toHaveBeenCalled();
+    expect(wrapper.state('allReservationsData')).toEqual(expected);
   });
 });
